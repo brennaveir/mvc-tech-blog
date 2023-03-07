441936de2a10b5fb9626e23adfe190c114c89684
brennaveir <brennaveir@hotmail.com>
2023-03-07 14:35:49 -0500

    added homepage and login views

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars')
 const config = require('./config/connection');
 const routes = require('./controllers');
 
 const app = express();
+const hbs = exphbs.create()
 const PORT = process.env.PORT || 3001;
 
 app.use(session(config.sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// app.use(routes);
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars')
+app.use(routes);
 
 config.sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
