3355a87b0849b0131e97cd81a9cb66971e27b27a
brennaveir <brennaveir@hotmail.com>
2023-03-13 17:33:25 -0400

    added comment js and route---NOT WORKING

---
+++ public/js/comment.js
@@ -0,0 +1,22 @@
+const newCommentHandler = async (event) => {
+    event.preventDefault();
+  
+    const comment = document.querySelector('#comment').value.trim();
+   
+  
+    if (comment) {
+      const response = await fetch(`/api/comment`, {
+        method: 'POST',
+        body: JSON.stringify({ comment }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to post comment');
+      }
+    }
+  };
