972263c4fc8265e78f30528fc2bde5c803ec1ec7
brennaveir <brennaveir@hotmail.com>
2023-03-14 02:06:20 -0400

    fixed failed to delete blogpost error that was actually deleting the blogpost

--- controllers/apiRoutes/blogpostRoutes.js
+++ controllers/apiRoutes/blogpostRoutes.js
@@ -47,7 +47,7 @@ router.delete('/:id', withAuth, async (req, res) => {
           return;
       }
 
-      res.status(200).json(projectData);
+      res.status(200).json(blogpostData);
   } catch (err) {
       res.status(500).json(err);
   }
