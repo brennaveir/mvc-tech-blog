5380b596435fa9888ac90d774347cea5e15e2594
brennaveir <brennaveir@hotmail.com>
2023-03-21 19:14:00 -0400

    posting comments works again

--- public/js/comment.js
+++ public/js/comment.js
@@ -14,7 +14,7 @@ const newCommentHandler = async (event) => {
       });
   
       if (response.ok) {
-        document.location.replace('/');
+        document.location.reload();
       } else {
         alert('Failed to post comment');
       }
